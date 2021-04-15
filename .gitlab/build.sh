6f70553f88cb1d37a4c822b8951954ddd45b3833
Kentleigh English <kentleigh@ikengainnovations.com>
2021-04-15 23:23:35 +0100

    Disable build

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -29,16 +29,16 @@ buildImage()
 	fi
 
 	echo "Logging in"
-	docker login rg.nl-ams.scw.cloud/ikenga -u $ACCESS_TOKEN -p $SECRET_TOKEN
+	#docker login rg.nl-ams.scw.cloud/ikenga -u $ACCESS_TOKEN -p $SECRET_TOKEN
 
 	echo "Building $name";
 	docker buildx build  --platform linux/arm/v7 --target=$@ --tag=$name .;
 
 	echo "Tagging..."
-	docker tag $name rg.nl-ams.scw.cloud/ikenga/$name
+	#docker tag $name rg.nl-ams.scw.cloud/ikenga/$name
 
 	echo "Pushing..."
-	docker push rg.nl-ams.scw.cloud/ikenga/$name
+	#docker push rg.nl-ams.scw.cloud/ikenga/$name
 	echo;
 }
 
